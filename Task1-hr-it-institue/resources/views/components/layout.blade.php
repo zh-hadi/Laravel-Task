2e67b2d1739ec9cc8932884b2ea35dad32cb5929
zh-hadi <zhhadi50@gmail.com>
2024-10-22 14:08:19 +0600

    fresh laravel project setup and view home page layout setup

---
+++ Task1-hr-it-institue/resources/views/components/layout.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Task 1</title>
+    @vite(['resources/css/app.css'])
+</head>
+<body>
+<div class="min-h-full">
+@include('components/navbar')
+
+  <header class="bg-white shadow">
+    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+      <h1 class="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
+    </div>
+  </header>
+  <main>
+    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+      {{ $slot }}
+    </div>
+  </main>
+</div>
+</body>
+</html>
---
+++ Task1-hr-it-institue/resources/views/home.blade.php
@@ -0,0 +1,3 @@
+<x-layout>
+    <h2>Hello world</h2>
+</x-layout>
