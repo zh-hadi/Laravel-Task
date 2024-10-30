8d8e61e498852581644b1fc00d66faa4a8d4790d
Showmen <showmen77barua@gmail.com>
2024-10-30 19:29:57 +0600

    Vendor List with product count

--- multi-vendor-site/app/Http/Controllers/Api/V1/Admin/AdminVendorController.php
+++ multi-vendor-site/app/Http/Controllers/Api/V1/Admin/AdminVendorController.php
@@ -6,11 +6,16 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\VendorListRequest;
 use App\Models\Vendor;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class AdminVendorController extends Controller
 {
     public function vendor_list(){
-        $vendors = Vendor::all();
+        $vendors = DB::table('vendors')
+                        ->join('products','products.vendor_id','=','vendors.id')
+                        ->select('vendors.*',DB::raw('count(products.vendor_id) as total_product'))
+                        ->groupBy('products.vendor_id')
+                        ->get();
         if($vendors){
             return response()->json($vendors);
         }
