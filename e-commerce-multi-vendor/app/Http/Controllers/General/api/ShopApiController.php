6148191d1b9de568b079ee2690819a34069fd03c
Showmen Barua <showmen77barua@gmail.com>
2024-12-03 17:48:26 +0600

    added api file

---
+++ e-commerce-multi-vendor/app/Http/Controllers/General/api/ShopApiController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\General\api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ShopApiController extends Controller
+{
+    public function cart_save(){
+        return 'test';
+    }
+}
